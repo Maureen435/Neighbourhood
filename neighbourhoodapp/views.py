0867d8ecc1166951322ca83c4138e300f2a07183
Maureen435 <mchepkemoi435@gmail.com>
2020-08-20 11:35:41 +0300

    added views

--- neighbourhoodapp/views.py
+++ neighbourhoodapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import RequestContext
+
+@csrf_protect
+def register(request):
+    # ...
+
+    return render_to_response(
+        'register.html',
+        {'form': form}, 
+        RequestContext(request)
+    )
