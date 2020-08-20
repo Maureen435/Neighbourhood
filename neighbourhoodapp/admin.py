15a7fc0281adc1cda4f253c278d7b9dfed37a0af
Maureen435 <mchepkemoi435@gmail.com>
2020-08-20 10:53:09 +0300

    registered admins

--- neighbourhoodapp/admin.py
+++ neighbourhoodapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile,Neighbourhood,Post,Business,NeighbourhoodAdmin,SystemAdmin,Contact#
+
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(NeighbourhoodAdmin)
+admin.site.register(SystemAdmin)
+admin.site.register(Contact)
