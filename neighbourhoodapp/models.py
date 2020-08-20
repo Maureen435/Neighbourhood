2d76861e47087be36b986888661ac5a5da112bf1
Maureen435 <mchepkemoi435@gmail.com>
2020-08-20 11:00:33 +0300

    added models

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -1,7 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.db.models.signnals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+@receiver(post_save, sender=User)
+def create_hood_admin(sender, instance, created, **kwargs):
+    if created:
+        NeighbourhoodAdmin.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_hood_admin(sender, instance, **kwargs):
+    instance.neighbourhoodadmin.save()
+@receiver(post_save, sender=User)
+def create_system_admin(sender, instance, created, **kwargs):
+    if created:
+        SystemAdmin.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_system_admin(sender, instance, **kwargs):
+    instance.systemadmin.save()
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupants_count = models.IntegerField(null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=50)
